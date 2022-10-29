c06a039d1727f77b9447018586798d4eed2d1753
Sawant Kumar <kumar.sawant37@gmail.com>
2022-10-29 11:56:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //global variable
 const time_el = document.querySelector('.watch .time');
-const start_btn = document.getElementById('start');
+const start_btn = document.getElementById("start");
 const stop_btn = document.getElementById("stop");
 const reset_btn = document.getElementById("reset");
+const text = document.querySelector(".text");
 
 let seconds = 0;
 let interval = null;
 
 //event listeners
-start_btn.addEventListener('click', start);
+start_btn.addEventListener("click", start);
 stop_btn.addEventListener("click", stop);
 reset_btn.addEventListener("click", reset);
 //update the timer
@@ -31,13 +32,22 @@ function start(){
         return
     }
     interval = setInterval(timer, 1000);
+    text.innerHTML=`
+    <p style="color:#7CFC00">Counter Started</p>
+    `;
 }
 function stop(){
     clearInterval(interval);
     interval = null;
+    text.innerHTML=`
+    <p style="color:red">Counter Stopped</p>
+    `;
 }
 function reset(){
     stop();
     seconds = 0;
     time_el.innerText = '00:00:00';
+    text.innerHTML=`
+    <p style="color:cyan">Counter Reset</p>
+    `;
 }
